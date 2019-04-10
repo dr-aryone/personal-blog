ed114ea8d8995820bcd9cdfd0eea6bd621455a9a
Fisher <jackfisher94@live.co.uk>
2019-04-10 16:36:14 +0100

    add auth to dashboard access

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-
+const { ensureAuthenticated } = require('../config/auth');
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.render('home');
 })
 
-router.get('/dashboard', (req, res) => {
-    res.render('dashboard');
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dashboard', {
+        // create variable name that contains users name to be used on the dashboard
+        name: req.user.name
+    });
 })
 
 module.exports = router;
