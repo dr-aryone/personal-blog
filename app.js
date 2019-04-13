1775b524896e1b6419468b8f84645f8c80867c29
Fisher <jackfisher94@live.co.uk>
2019-04-13 22:41:59 +0100

    add mongodb dependency

--- app.js
+++ app.js
@@ -53,12 +53,6 @@ app.use((req, res, next) => {
     next();
 });
 
-require('dotenv').config();
-
-const dbpass = process.env.DB_PASS;
-const mlabpass = process.env.MLAB_PASS;
-
-
 app.use(express.static('public'));
 
 // External routes
