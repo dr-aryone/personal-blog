4d312304727fde145ac228739ae8cf08c45a1c7f
Fisher <jackfisher94@live.co.uk>
2019-04-17 16:32:02 +0100

    initial commit

--- models/Article.js
+++ models/Article.js
@@ -3,19 +3,28 @@ const mongoose = require('mongoose');
 const ArticleSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        minlength: 4,
+        maxlength: 255,
+        trim: true,
     },
     author: {
         type: String,
-        required: true
+        required: true,
+        minlength: 2,
+        maxlength: 255,
+        trim: true,
     },
     time: {
         type: Date,
-        default: Date.now
+        default: Date.now,
+        required: true,
     },
     body: {
         type: String,
-        required: true
+        required: true,
+        minlength: 2,
+        maxlength: 5000,
     },
 });
 
