1a30c57d8615771870448472a6bee834d5111135
Fisher <jackfisher94@live.co.uk>
2019-04-20 10:49:57 +0100

    Add are you sure modal to delete button

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,8 @@
 let articleLink = document.querySelectorAll('.article-link');
 let articleTitle = document.querySelectorAll('.title')
+const deleteButton = document.querySelectorAll('.delete-button');
+const confirmDeleteModal = document.querySelectorAll('.confirm-delete-modal');
+const cancelDelete = document.querySelectorAll('.cancel-delete');
 
 window.addEventListener('load', setHREF);
 
@@ -10,10 +13,27 @@ function setHREF() {
         titleList.push(title.innerText.split(' ').join('-'));
     })
 
-    console.log(titleList);
-
     for(var i=0; i< titleList.length; i++) {
         let link = titleList[i];
         articleLink[i].setAttribute('href', `/blogs/${link}`)
     }
 }
+
+// display modal when delete button clicked on dashboard article 
+for(var i =0; i < deleteButton.length; i++) {
+    deleteButton[i].addEventListener('click', () => {
+        for(var j = 0; j < confirmDeleteModal.length; j++) {
+            confirmDeleteModal[j].style.display = 'flex';
+        }
+})
+}
+
+// remove modal when cancel button clicked on dashboard article
+for (var i = 0; i < cancelDelete.length; i++) {
+cancelDelete[i].addEventListener('click', () => {
+    for (var j = 0; j < confirmDeleteModal.length; j++) {
+        confirmDeleteModal[j].style.display = 'none';
+    }
+})
+}
+
