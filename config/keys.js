3c013971b0be2da68e733ad5427e9bcab80f4fc4
Fisher <jackfisher94@live.co.uk>
2019-05-08 19:07:35 +0100

    revert back to proper db

--- config/keys.js
+++ config/keys.js
@@ -3,12 +3,11 @@ require('dotenv').config();
 const dbpass = process.env.DB_PASS;
 const mlabpass = process.env.MLAB_PASS;
 const jwtPrivateKey = process.env.jwtPrivateKey;
-const testPass = process.env.TEST_PASS;
 
 
-MongoURI = "mongodb+srv://fishj123:" + testPass + "@vidlycluster-hcnja.azure.mongodb.net/test?retryWrites=true",
-    
-// "mongodb://fishj123:" + mlabpass + "@ds149885.mlab.com:49885/heroku_26921dvn"
+
+MongoURI = "mongodb://fishj123:" + mlabpass + "@ds149885.mlab.com:49885/heroku_26921dvn",
+  
 
 
 module.exports.jwtPrivateKey = jwtPrivateKey;
