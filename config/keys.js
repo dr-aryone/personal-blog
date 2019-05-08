b6ebb4f8ab006a16f7b255c35dffec8eff10a0b7
Fisher <jackfisher94@live.co.uk>
2019-05-08 19:05:41 +0100

    test 2

--- config/keys.js
+++ config/keys.js
@@ -3,9 +3,10 @@ require('dotenv').config();
 const dbpass = process.env.DB_PASS;
 const mlabpass = process.env.MLAB_PASS;
 const jwtPrivateKey = process.env.jwtPrivateKey;
+const testPass = process.env.TEST_PASS;
 
 
-MongoURI = "mongodb+srv://fishj123:Hawaii5882@vidlycluster-hcnja.azure.mongodb.net/test?retryWrites=true",
+MongoURI = "mongodb+srv://fishj123:" + testPass + "@vidlycluster-hcnja.azure.mongodb.net/test?retryWrites=true",
     
 // "mongodb://fishj123:" + mlabpass + "@ds149885.mlab.com:49885/heroku_26921dvn"
 
